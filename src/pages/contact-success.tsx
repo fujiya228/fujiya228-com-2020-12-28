4bd90101d3b26e68d5bf188b7ebcb676fa750152
fujiya228 <fujiya0xE2@gmail.com>
2020-12-30 15:02:13 +0900

    [add] ContactSucess

---
+++ src/pages/contact-success.tsx
@@ -0,0 +1,140 @@
+import { graphql, Link } from 'gatsby';
+import React from 'react';
+
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import SiteNav from '../components/header/SiteNav';
+import { PostCard } from '../components/PostCard';
+import { Footer } from '../components/Footer';
+import { Wrapper } from '../components/Wrapper';
+import IndexLayout from '../layouts';
+import { colors } from '../styles/colors';
+import { inner, outer, PostFeed, SiteHeader, SiteNavMain } from '../styles/shared';
+import { PageContext } from '../templates/post';
+
+interface ContactSuccessTemplateProps {
+  data: {
+    allMarkdownRemark: {
+      totalCount: number;
+      edges: Array<{
+        node: PageContext;
+      }>;
+    };
+  };
+}
+
+const ContactSuccessPage: React.FC<ContactSuccessTemplateProps> = props => {
+  const { edges } = props.data.allMarkdownRemark;
+
+  return (
+    <IndexLayout>
+      <Wrapper>
+        <header css={[SiteHeader, outer]}>
+          <div css={[outer, SiteNavMain]}>
+            <div css={inner}>
+              <SiteNav isHome />
+            </div>
+          </div>
+        </header>
+        <main id="site-main" css={[outer, SuccessContent]} className="success-content">
+          <div css={[inner]}>
+            <section style={{ textAlign: 'center' }}>
+              <ThanksMessage>
+                お問い合わせありがとうございます！
+              </ThanksMessage>
+              <Link to="/">
+                Topページへ移動 ⇒
+              </Link>
+            </section>
+
+            <div css={PostFeed} className="post-feed">
+              {edges.map(({ node }) => (
+                <PostCard key={node.fields.slug} post={node} />
+              ))}
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </Wrapper>
+    </IndexLayout>
+  );
+};
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(limit: 3, sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          timeToRead
+          frontmatter {
+            title
+            date
+            tags
+            image {
+              childImageSharp {
+                fluid(maxWidth: 3720) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            author {
+              id
+              bio
+              avatar {
+                children {
+                  ... on ImageSharp {
+                    fluid(quality: 100, srcSetBreakpoints: [40, 80, 120]) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+            }
+          }
+          excerpt
+          fields {
+            layout
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+const SuccessContent = css`
+  padding: 14vw 4vw 6vw;
+
+  @media (max-width: 800px) {
+    padding-top: 24vw;
+  }
+
+  @media (max-width: 500px) {
+    padding-top: 28vw;
+  }
+
+  @media (min-width: 940px) {
+    .post-card {
+      margin-bottom: 0;
+      padding-bottom: 0;
+      border-bottom: none;
+    }
+  }
+`;
+
+const ThanksMessage = styled.p`
+  margin: 32px 0;
+  /* color: var(--midgrey); */
+  color: ${colors.midgrey};
+  font-size: 3rem;
+  line-height: 1.3em;
+  font-weight: 400;
+
+  @media (max-width: 800px) {
+    margin: 5px 0 0 0;
+    font-size: 1.8rem;
+  }
+`;
+
+export default ContactSuccessPage;
