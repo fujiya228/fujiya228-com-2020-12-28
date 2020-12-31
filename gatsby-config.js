003a0991b527602b6a651b04f58e69225f551427
fujiya228 <fujiya0xE2@gmail.com>
2021-01-01 03:41:26 +0900

    [mod] google analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-139356271-1',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
