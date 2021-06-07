ad9f2ab593c60e51ed67cced9aae17a974d3e793
fujiya228m <fujiya0xE2@gmail.com>
2021-06-07 23:56:00 +0900

    [fix]_adsense_error

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -19,19 +19,19 @@ export const Adsense: React.FC<AdsenseProps> = props => {
     }
   }, [props.pathname]);
   const client = 'ca-pub-5351220307196429';
+  const format = 'fluid';
 
   const layout = props.isInfead ? null : 'in-article';
   const layoutKey = props.isInfead ? '-6t+ed+2i-1n-4w' : null;
   const slot = props.isInfead ? '5405028860' : '1004093357';
-  const style = props.isInfead ? 'min-width: 250px' : null;
 
   return (
     <Ins
       className="adsbygoogle"
-      css={style}
       data-ad-client={client}
       data-ad-layout={layout}
       data-ad-layout-key={layoutKey}
+      data-ad-format={format}
       data-ad-slot={slot}
     />
   );
@@ -40,4 +40,5 @@ export const Adsense: React.FC<AdsenseProps> = props => {
 const Ins = styled.ins`
   display:block;
   text-align:center;
+  min-width: 250px;
 `;
